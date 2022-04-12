947db227c2269012f408d018ac6cd13a06ccd861
mugimugi <78909330+mugimugi555@users.noreply.github.com>
2022-04-12 12:12:34 +0900

    Update install_jquake.sh

--- install_jquake.sh
+++ install_jquake.sh
@@ -23,6 +23,7 @@ sudo apt install -y wmctrl ;
 
 env | grep DISPLAY
 
+MY_SCREENSHOT_JQUAKE=$(cat<<TEXT
 #!/usr/bin/bash
 
 export DISPLAY=:0 ;
@@ -30,3 +31,10 @@ wmctrl -a "JQuake" ;
 gnome-screenshot -w -B -e none -f /var/www/html/earthquake/capture.png ;
 #convert /var/www/html/earthquake/capture.png -crop 728x480+10+45 /var/www/html/earthquake/capture_triming.png ;
 convert /var/www/html/earthquake/capture.png  /var/www/html/earthquake/capture_triming.png ;
+TEXT
+)
+echo "$MY_SCREENSHOT_JQUAKE" > do_screenshot.sh ;
+
+#
+chmod +x do_screenshot.sh ;
+./do_screenshot.sh;
