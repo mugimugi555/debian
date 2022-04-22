4f84f339539d68a1e55d176aa1f4b1332142184a
mugimugi <78909330+mugimugi555@users.noreply.github.com>
2022-04-22 19:12:25 +0900

    Update install_vnc_viewer.sh

--- Q584_H/install_vnc_viewer.sh
+++ Q584_H/install_vnc_viewer.sh
@@ -2,6 +2,9 @@
 
 # wget https://raw.githubusercontent.com/mugimugi555/debian/main/Q584_H/install_vnc_viewer.sh && bash install_vnc_viewer.sh ;
 
-echo 'deb http://ftp.debian.org/debian stretch-backports main' | sudo tee --append /etc/apt/sources.list.d/stretch-backports.list >> /dev/null ;
-sudo apt update ;
-sudo apt install -t stretch-backports remmina remmina-plugin-rdp remmina-plugin-secret remmina-plugin-spice ;
+sudo echo ;
+sudo snap install remmina ;
+
+#echo 'deb http://ftp.debian.org/debian stretch-backports main' | sudo tee --append /etc/apt/sources.list.d/stretch-backports.list >> /dev/null ;
+#sudo apt update ;
+#sudo apt install -t stretch-backports remmina remmina-plugin-rdp remmina-plugin-secret remmina-plugin-spice ;
